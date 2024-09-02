083ac2b411fd698f8f6a1dda1e1b07f759c40504
gamalgithue <gamal_678a@fci.kfs.edu.eg>
2024-09-03 00:25:45 +0300

    Intial Commit

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=JOO-PC\\MSSQLSERVER2022;database=Bulky_DB;integrated security = true;MultipleActiveResultSets=True;TrustServerCertificate=true;");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+
+                );
+        }
+        public DbSet<Category> Categories { get; set; }
+        
+
+    }
+}
---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage ="Category Name is Required")]
+        [DisplayName("Category Name")]
+        [MinLength(3)]
+        
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Range between 1 and 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
